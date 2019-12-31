814ce01d6cece90b79c3e126b704a16594cf8a0e
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2019-12-31 12:02:01 -0300

    Layout página de autenticação

--- src/pages/Auth/SignIn/index.js
+++ src/pages/Auth/SignIn/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import Button from '~/styles/components/Button';
+import { Container, SignForm } from '../styles';
 
 export default function SignIn() {
-  return <div />;
+  return (
+    <Container>
+      <SignForm onSubmit={() => {}}>
+        <h1>Boas vindas</h1>
+
+        <span>E-MAIL</span>
+        <input type="email" name="email" />
+
+        <span>SENHA</span>
+        <input type="password" name="password" />
+
+        <Button size="big" type="submit">
+          Entrar
+        </Button>
+      </SignForm>
+    </Container>
+  );
 }
